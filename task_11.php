e2d38f99fef44c314a561e51ff5e754d2b272579
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2021-02-18 16:52:45 +0600

    Update task_11.php

--- task_11.php
+++ task_11.php
@@ -1,19 +1,3 @@
-<?php 
-
-$name_file = trim(mb_strtolower($_FILES['file']['name']));
-$tmp_name = $_FILES['file']['tmp_name'];
-if(!file_exists('img')){
-mkdir('img');
-}
-$filename='img/$name_file';
-move_uploaded_file($tmp_name,$filename);
-
-if (file_exists($filename) && !empty($_FILES)){
-	echo 'Файл сохранено' . $name_file;
-}
-
- ?>
-
 <html>
  <form action="save_11.php" enctype="multipart/form-data" method="post">
   
