fc5b53f640267277197630a43b38ad24adb454a9
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2021-02-18 16:49:12 +0600

    Update save_11.php

--- save_11.php
+++ save_11.php
@@ -1,7 +1,11 @@
 <?php 
+
 $username = $_POST['username'];
+$email =$_POST['email'];
 $pdo = new PDO ("mysql:host=localhost;dbname=my_study;","mysql","mysql");
-$sql= INSERT INTO task (username) VALUES (:username);
+$sql= "INSERT INTO task (username,email) VALUES (:username,:email)";
 $statement = $pdo->prepare($sql);
-$statement -> execute(['username'=>$username]);
+$statement -> execute(['username'=>$username,'email'=>$email]);
+
+
  ?>
