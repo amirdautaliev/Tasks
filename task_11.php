8e48f96865659ba26a833e7f1c7df6cb6c181e5e
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2021-02-20 10:32:02 +0600

    Update task_11.php

--- task_11.php
+++ task_11.php
@@ -1,3 +1,22 @@
+<?php 
+if(!empty($_FILES['img']['tmp_name']))
+	$img = addslashes(file_get_contents($_FILES['img']['tmp_name']));
+
+
+/*$name_file = trim(mb_strtolower($_FILES['file']['name']));
+$tmp_name = $_FILES['file']['tmp_name'];
+if(!file_exists('img')){
+mkdir('img');
+}
+$filename='img/$name_file';
+move_uploaded_file($tmp_name,$filename);
+
+if (file_exists($filename) && !empty($_FILES)){
+	echo 'Файл сохранено' . $name_file;
+}*/
+
+ ?>
+
 <html>
  <form action="save_11.php" enctype="multipart/form-data" method="post">
   
@@ -5,7 +24,7 @@
  <legend>информация</legend>
  <label>Имя<input type="username" name='username'></label>
  <label>E-mail<input type="email"name='email' ></label>
- <label> Отправить файл <input type="file" name='file'></label>
+ <label> Отправить файл <input type="file" name='img'></label>
  <label><input type="submit" value='Отправить'></label>
  </fieldset>
 
