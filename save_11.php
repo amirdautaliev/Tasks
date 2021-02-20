fe3c0dd28b5bd7e54dea48b6a2f096b7a104bce5
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2021-02-20 10:32:56 +0600

    Update save_11.php

--- save_11.php
+++ save_11.php
@@ -1,11 +1,14 @@
 <?php 
-
 $username = $_POST['username'];
 $email =$_POST['email'];
+$img = $_FILES ['img'];
+$tmp_name = $_FILES['img'];
+
 $pdo = new PDO ("mysql:host=localhost;dbname=my_study;","mysql","mysql");
-$sql= "INSERT INTO task (username,email) VALUES (:username,:email)";
+$sql= "INSERT INTO task (username,email,img) VALUES (:username,:email)";
 $statement = $pdo->prepare($sql);
 $statement -> execute(['username'=>$username,'email'=>$email]);
-
-
+$connection->query('INSERT INTO task VALUES ('$img''));
+//header('Location: task_11.php');
+//exit();
  ?>
