569c41ae3cd070b9449697ed606a1f8502ac3d7b
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2021-02-24 00:36:00 +0600

    Update save_11.php

--- save_11.php
+++ save_11.php
@@ -1,16 +1,17 @@
 <?php 
-
+require 'task_11.php';
 $username = $_POST['username'];
 $email =$_POST['email'];
-$imagetmp = $_FILES ['image']['tmp_name'];
-$imagename = $_FILES['image']['name'];
+$tmp_name = $_FILES ['image']['tmp_name'];
+$name = $_FILES['image']['name'];
 $path = 'uploads/';
-$filedir = $path.$imagetmp;
+$filedir = $path.$tmp_name;
 $pdo = new PDO ("mysql:host=localhost;dbname=my_study;","mysql","mysql");
 $sql= "INSERT INTO task (username,email,image_path) VALUES (:username, :email, :image_path)";
 $statement = $pdo->prepare($sql);
 $statement -> execute(['username'=>$username,'email'=>$email,'image_path'=>$filedir]);
-header('Location: task_11.php');
-  uploadImage($_FILES['image']);
-//exit();
+//header('Location: task_11.php');
+//move_uploaded_file($tmp_name,$filedir);
+  uploadImage($image);
+//exit();	
  ?>
