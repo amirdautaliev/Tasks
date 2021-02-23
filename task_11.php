02314b718e3565fc64faa6f32d417ba936a276dc
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2021-02-24 00:14:26 +0600

    Update task_11.php

--- task_11.php
+++ task_11.php
@@ -1,6 +1,14 @@
  <?php 
-require 'uploads.php';
-  uploadImage($_FILES['image']);
+
+	function uploadImage($image)
+	{
+	$name = $image['name'];
+	$tmp_name =$_FILES['image']['tmp_name'];
+	move_uploaded_file($tmp_name, 'uploads/'. $name);
+	 }
+	  uploadImage($_FILES['image']);
+
+
   ?>
  <html>
  <form action="save_11.php" enctype="multipart/form-data" method="post">
