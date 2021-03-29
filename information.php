30a69a7916ad550ccc49eb9b352df55fd1e82e29
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2021-03-30 01:47:33 +0600

    Add files via upload

---
+++ information.php
@@ -0,0 +1,16 @@
+<form>
+ <html>
+ <form action="save_11.php" enctype="multipart/form-data" method="post">
+                                 <fieldset>
+ <legend>информация</legend>
+ <label for="username">Логин:</label>
+ <input type="text" required id="username" name="username" />
+ <label for="email">email</label>
+  <input type="text" required id="email" name="email" />
+ <label> Отправить файл <input type="file" name='image'></label>
+ <label><input type="submit" value='Отправить'></label>
+ </fieldset>
+</form>
+</html>
+
+</form>
