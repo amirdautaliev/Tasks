03ba28e780feb8c039042ddee0b06e0122399324
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2021-10-30 02:25:57 +0600

    Update registr.php

--- registr.php
+++ registr.php
@@ -2,18 +2,32 @@
 session_start();
 $email = $_POST["email"];
 $password = $_POST["password"];
+function check_user($email){
 $pdo = new PDO ("mysql:host=localhost;dbname=project","mysql","mysql");
  $sql = "SELECT * FROM users WHERE email=:email";
  $statement=$pdo->prepare($sql);
  $statement->execute(["email"=>$email]);
  $user = $statement->fetch(PDO::FETCH_ASSOC);
+ return($user);
+}
 
- if(!empty($user)) {
+$user = check_user($email);	
+
+
+  
+ 	if(!empty($user)){
  $_SESSION["danger"]="Этот эл. адрес уже занят другим пользователем.";
+
  header("Location: /page_register.php"); 
  exit;
  }
 
+ else $_SESSION["success"] = "Регистрация успешна прошло, можете заходить!";{
+header("location: /page_login.php");
+}
+ function add_user($email,$password)
+ {
+ 	$pdo = new PDO ("mysql:host=localhost;dbname=project","mysql","mysql");
  $sql = "INSERT INTO users (email,password) VALUES (:email,:password)";
  $statement=$pdo->prepare($sql);
  $statement->execute([
@@ -21,6 +35,11 @@ $pdo = new PDO ("mysql:host=localhost;dbname=project","mysql","mysql");
 "password"=>$password
 
 ]);
-$_SESSION["success"] = "Регистрация успешна прошло, можете заходить!";
-header("location: /page_login.php");
+}
+
+
+add_user($email,$password);
+
+
+
  ?>
