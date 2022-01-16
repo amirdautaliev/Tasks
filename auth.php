dff753bbc569755bdc447fc45a83861edcbfb204
amirdautaliev <59817590+amirdautaliev@users.noreply.github.com>
2022-01-17 00:32:59 +0600

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,18 +1,18 @@
 <?php 	
 session_start();
-function is_user_logged_in(){
+function is_user_logged_in(){       // Функция проверки существует ли пользователь
  return isset($_SESSION['user']);
    }
    
    function login_require(){
-    if (!is_user_logged_in()){
+    if (!is_user_logged_in()){                  // Проверка страницы на авторизация
         header("Location: /page_login.php");
     }
    }
    
-function logout(){
+function logout(){                                  // Функция для удаления сессий  выхода
     if(is_user_logged_in()) {
-        unset($_SESSION['user']);
+        unset($_SESSION['user']);  
         session_destroy();
         header("Location: /page_login.php");
     }
